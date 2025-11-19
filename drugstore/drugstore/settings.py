5134dc4224617ca6a7f6659508bf106ffd6b3195
Edson Melo <edsonmelonf@gmail.com>
2025-11-19 17:38:16 -0300

    feat: update login and register pages

--- drugstore/drugstore/settings.py
+++ drugstore/drugstore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users'
+  
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
--- drugstore/users/views.py
+++ drugstore/users/views.py
@@ -1,52 +1,37 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from .forms import RegisterForm
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')  # home
+    else:
+        form = RegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
+
+
 
 def login_view(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-        user = authenticate(username=username, password=password)
 
-        if user:
+        user = authenticate(request, username=username, password=password)
+
+
+        if user is not None:
             login(request, user)
-            return redirect("/")  
+
+            return redirect('/') #home.html
+        
         else:
-            return render(request, "users/login.html", {
-                "error": "Usuário ou senha incorretos"
-            })
+            return render(request, 'login.html', {'error' : 'Credenciais inválidas'})
+        
 
-    return render(request, "users/login.html")
-
-
-def register_user(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        confirm = request.POST.get("confirm")
-
-    
-        if password != confirm:
-            return render(request, 'users/register.html', {
-                'error': 'As senhas não coincidem'
-            })
-
-
-        if User.objects.filter(username=username).exists():
-            return render(request, 'users/register.html', {
-                'error': 'Usuário já existe'
-            })
-
-        user = User.objects.create_user(
-            username=username,
-            email=email,
-            password=password
-        )
-        user.save()
-
-        return redirect('users:login')
-
-    return render(request, 'users/register.html')
+    return render(request,'users/login.html')
