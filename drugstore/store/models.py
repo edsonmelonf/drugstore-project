283538286bb9dbde782baf5dc360fc0999d449ce
Edson Melo <edsonmelonf@gmail.com>
2025-11-20 20:11:38 -0300

    chore: testing product fields

--- drugstore/store/models.py
+++ drugstore/store/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Product(models.Model):
     name_product = models.CharField(max_length=200)
-    image_product = models.ImageField(upload_to='products')
+    image_product = models.ImageField(upload_to='media/products/')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
